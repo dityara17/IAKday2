348bf1b46f277dc33b6f7bf201f578d665b7e058
dityara17 <arizki717@gmail.com>
2018-01-27 23:05:45 +0700

    one of me

---
+++ app/src/main/java/com/dityaranote/menumakanan/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dityaranote.menumakanan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
